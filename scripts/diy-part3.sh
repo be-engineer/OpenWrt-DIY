74d0ff7145eaea1198816aae54b0c0413be48528
leon <41234995@qq.com>
2021-03-24 14:57:57 +0800

    luci-app-adguardhome的clone路径还是不对，再次修改

--- scripts/diy-part3.sh
+++ scripts/diy-part3.sh
@@ -45,7 +45,7 @@ git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
 
 # Add luci-app-adguardhome
-git clone --depth=1 https://github.com/be-engineer/luci-app-adguardhome ./package/
+git clone --depth=1 https://github.com/be-engineer/luci-app-adguardhome ./package/luci-app-adguardhome
 
 # Add luci-app-diskman
 git clone --depth=1 https://github.com/SuLingGG/luci-app-diskman
